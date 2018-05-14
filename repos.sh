027a57fab48022dcf35e6a8efe3f464ce2a89233
François Michonneau <francois.michonneau@gmail.com>
2018-05-14 17:52:07 -0400

    [fix #2] add SWC-ES and DC repositorie

--- repos.sh
+++ repos.sh
@@ -1,6 +1,7 @@
 STYLES_REPO="styles"
 
-LESSONS_REPOS="git-novice
+SWC_LESSONS_REPOS="git-novice
+git-novice-es
 hg-novice
 hpc-novice
 instructor-training
@@ -9,8 +10,39 @@ matlab-novice-inflammation
 python-novice-gapminder
 python-novice-inflammation
 r-novice-gapminder
+r-novice-gapminder-es
 r-novice-inflammation
 shell-novice
+shell-novice-es
 sql-novice-survey"
+
+DC_LESSONS_REPOS="ecology-workshop
+openrefine-ecology-lesson
+python-ecology-lesson
+spreadsheet-ecology-lesson
+sql-ecology-lesson
+geospatial-workshop
+organization-geospatial
+r-intro-geospatial
+r-raster-vector-geospatial
+openrefine-socialsci
+python-socialsci
+r-socialsci
+socialsci-workshop
+spreadsheet-socialsci
+sql-socialsci
+cloud-genomics
+genomics-workshop
+organization-genomics
+R-genomics
+shell-genomics
+wrangling-genomics
+rr-automation
+rr-intro
+rr-literate-programming
+rr-organization1
+rr-publication
+rr-version-control
+rr-workshop"
 
 ALL_REPOS="$STYLES_REPO $LESSONS_REPOS"
